bb83d94cd47ebbfbdedc241d49565e9b6f842f08
Shahk_dev <shahkdev07@example.com>
2025-11-01 23:37:37 +0500

    Notfound

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Notfound() {
   return (
-    <div className='bg-[#202020] h-screen text-while'>
-          404
+    <div className='bg-[#202020] h-screen flex item-center justify-center text-while'>
+           <div>
+               <p className='text-[250px] mt-30 ml-35 font-bold text-white'>404</p>
+               <p className='text-lg text-center text-white w-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi asperiores veritatis praesentium tempora culpa voluptate inventore dolore eius aliquid, vitae dolor expedita accusamus suscipit reiciendis cumque. Veritatis, qui ratione. Nihil?</p>
+           </div>
     </div>
   )
 }
