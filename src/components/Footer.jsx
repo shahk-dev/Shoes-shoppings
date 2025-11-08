30b6e38d752fb2492c015e543f4a26f02bedbe5f
𝕾𝖍𝖆𝖝𝖗𝖎𝖞𝖔𝖗 𝕿𝖚𝖗𝖌'𝖚𝖓𝖔𝖛 <shaxriyorturgunov000@gmail.com>
2025-11-08 16:50:27 +0500

    dsd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-         
+         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis nisi corporis ab nihil expedita, quo similique laborum modi porro ex quos dolores. Doloremque aperiam obcaecati ipsa? Adipisci consequatur veritatis autem.</p>
     </div>
   )
 }
